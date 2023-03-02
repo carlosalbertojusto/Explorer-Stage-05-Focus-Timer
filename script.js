d36e66aa368054bb2e39c3309b60414d33b73e06
carlosalbertojusto <carlosjustodev@gmail.com>
2023-03-02 18:23:47 -0400

    :tada: add: first design

---
+++ script.js
@@ -0,0 +1,102 @@
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+let play = document.querySelector(".play")
+let stop = document.querySelector(".stop")
+const addMoreTime = document.querySelector(".addTime")
+const decreaseTime = document.querySelector(".decreaseTime")
+const buttonSoundRain = document.querySelector(".rain")
+const buttonSoundTree = document.querySelector(".tree")
+const buttonSoundFireplace = document.querySelector(".fireplace")
+const buttonSoundCoffeeShop = document.querySelector(".coffeeShop")
+let timerTimeOut
+let newMinutes
+let newSeconds
+let isFocused
+let rain = new Audio("./sounds/Chuva.wav")
+let tree = new Audio("./sounds/Floresta.wav")
+let fireplace = new Audio("./sounds/Lareira.wav")
+let coffeeShop = new Audio("./sounds/Cafeteria.wav")
+
+function playTimer() {
+  timerTimeOut = setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    if (seconds <= 0) {
+      seconds = 60
+      minutesDisplay.textContent = String(--minutes).padStart(2, "0")
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    playTimer()
+  }, 1000)
+}
+
+function stopTimer() {
+  clearTimeout(timerTimeOut)
+  newMinutes = 25
+  newSeconds = 0
+  minutesDisplay.textContent = String(newMinutes)
+  secondsDisplay.textContent = String(newSeconds).padStart(2, "0")
+}
+
+function addMoreMinutes() {
+  minutes = Number(minutesDisplay.textContent)
+  newMinutes = 5 + minutes
+  minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
+}
+
+function decreaseTimer() {
+  minutes = Number(minutesDisplay.textContent)
+  newMinutes = minutes - 5
+  minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
+  if (newMinutes <= 0) {
+    minutesDisplay.textContent = String(0).padStart(2, "0")
+  }
+}
+
+function playSoundRain() {
+  rain.play()
+}
+
+function stopSoundRain() {
+  rain.pause()
+}
+
+function playSoundTree() {
+  tree.play()
+}
+
+function stopSoundTree() {
+  tree.pause()
+}
+
+function playSoundFireplace() {
+  fireplace.play()
+}
+
+function stopSoundFireplace() {
+  fireplace.pause()
+}
+
+function playSoundCoffeeShop() {
+  coffeeShop.play()
+}
+
+function stopSoundCoffeeShop() {
+  coffeeShop.pause()
+}
+
+play.addEventListener("click", playTimer)
+stop.addEventListener("click", stopTimer)
+addMoreTime.addEventListener("click", addMoreMinutes)
+decreaseTime.addEventListener("click", decreaseTimer)
+
+buttonSoundRain.addEventListener("focus", playSoundRain)
+buttonSoundTree.addEventListener("focus", playSoundTree)
+buttonSoundFireplace.addEventListener("focus", playSoundFireplace)
+buttonSoundCoffeeShop.addEventListener("focus", playSoundCoffeeShop)
+
+buttonSoundRain.addEventListener("blur", stopSoundRain)
+buttonSoundTree.addEventListener("blur", stopSoundTree)
+buttonSoundFireplace.addEventListener("blur", stopSoundFireplace)
+buttonSoundCoffeeShop.addEventListener("blur", stopSoundCoffeeShop)
